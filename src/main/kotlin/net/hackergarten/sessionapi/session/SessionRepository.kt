729bfcaf50769e125489369d6bcca380ccfd137c
Florent Biville <fbiville@pivotal.io>
2019-06-11 22:31:07 +0200

    Remove unneeded annotation

--- src/main/kotlin/net/hackergarten/sessionapi/session/SessionRepository.kt
+++ src/main/kotlin/net/hackergarten/sessionapi/session/SessionRepository.kt
@@ -1,10 +1,5 @@
 package net.hackergarten.sessionapi.session
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource(collectionResourceRel = "sessions", path = "sessions")
-interface SessionRepository : JpaRepository<Session, Long>{
-
-
-}
+interface SessionRepository : JpaRepository<Session, Long>
